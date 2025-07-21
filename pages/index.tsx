8455b2c1eeaf3811ff64c112371744e57436aca4
Will D <diweizhi@gmail.com>
2025-07-21 16:02:21 +1000

    Static Route

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1>hello next</h1>
+      <h1>hello ni</h1>
     </main>
   );
 }
---
+++ pages/posts/detail.tsx
@@ -0,0 +1,9 @@
+const detail = () => {
+  return (
+    <main>
+      <h1>Detail page</h1>
+    </main>
+  );
+};
+
+export default detail;
